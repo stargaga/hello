0716903479a907be6561145f0482015d3804ed31
stargaga <112456931+stargaga@users.noreply.github.com>
2023-07-25 19:28:42 +0800

    Update GitTest.java

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello,git4");
         System.out.println("hello.git5");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
