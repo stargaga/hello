e4a7723867a0a1f60b677c432c9dd7cdf6416e81
CryingBee <ao.gu@outlook.com>
2023-07-25 18:53:12 +0800

    hox-fix commit2

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello,git!");
         System.out.println("hello,git1!");
         System.out.println("hello,git4");
+        System.out.println("hello.git5");
     }
 }
 
