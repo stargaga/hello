d5e852f6540b0bf4bc88e2ab6cb3b36f0511df13
CryingBee <ao.gu@outlook.com>
2023-07-25 18:17:24 +0800

    second commit

--- src/main/java/git/GitTest.java
+++ src/main/java/git/GitTest.java
@@ -1,7 +1,14 @@
 package git;
-
+/**
+ * @Author CryingBee
+ * @Desctription TODO
+ * @Date 2023/7/25 18:14
+ * @since JDK 1.8
+ *
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git1!");
     }
 }
